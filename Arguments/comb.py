0e0f072b858656c652a6c30eaffc59c24e04f524
Biswa20056 <nayakbiswajit295@gmail.com>
2026-09-03 21:51:20 +0530

    Added combination of variable length keyword and variable length nokeyword

---
+++ Arguments/comb.py
@@ -0,0 +1,5 @@
+def student(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+
+student("Biswajit", 22, "B.Tech", city="Bengaluru", skill="Python")
