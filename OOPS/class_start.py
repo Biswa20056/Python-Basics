c32751fc31691d22c5437fa8a501f8df27c0bd2a
Biswa20056 <nayakbiswajit295@gmail.com>
2026-09-10 20:47:20 +0530

    Added OOPS Starting

---
+++ OOPS/class_start.py
@@ -0,0 +1,8 @@
+class sample:
+    height = 6.0
+    location = 'Bhubaneswar'
+obj1 = sample()
+obj2 = sample()
+print(obj1.height)
+print(obj1.height, obj1.location)
+print(sample.height, sample.location)
